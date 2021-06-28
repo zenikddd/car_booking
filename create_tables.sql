e511f95237c48998e25752e351699bd6fc491241
sergey <gmyf32@gmail.com>
2021-06-28 10:55:57 +0300

    Change create  table script

--- create_tables.sql
+++ create_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE "car"
 	"model"         character varying(300) NOT NULL,
 	"license_plate" character varying(300) NOT NULL,
 	"vin"           character varying(300) NOT NULL,
+    "register_date" date NOT NULL,
 	CONSTRAINT "car_pk" PRIMARY KEY ("id")
 ) WITH (
 	  OIDS = FALSE
@@ -55,4 +56,5 @@ ALTER TABLE "car_booking"
 
 INSERT INTO rate(distance, cost) VALUES (200, 270), (350, 330), (500, 390);
 INSERT INTO discount(rate, from_days, to_days) VALUES (5, 3, 5), (10, 6, 14), (15, 15, 30);
-INSERT INTO car(brand, model, license_plate, vin) VALUES ('tesla', 'model x', 'Т033ТМ', '1FMCU0E71CKB86966'), ('bmw', 'i8', 'А327РК', '1HGEJ6600TL060913'), ('toyota', 'supra', 'Т630УС', 'JF2SJGUC2FH513006'), ('mercedes-benz', 'g-class', 'В547РС', '2G1FP22G922147195'), ('honda', 'civic', 'О605КХ', '2HGFG21547H776568');
+INSERT INTO car(brand, model, license_plate, vin, register_date) VALUES ('tesla', 'model x', 'Т033ТМ', '1FMCU0E71CKB86966', '2021-01-01'), ('bmw', 'i8', 'А327РК', '1HGEJ6600TL060913', '2021-03-01'), ('toyota', 'supra', 'Т630УС', 'JF2SJGUC2FH513006', '2021-05-01'), ('mercedes-benz', 'g-class', 'В547РС', '2G1FP22G922147195', '2021-05-15'), ('honda', 'civic', 'О605КХ', '2HGFG21547H776568', '2021-06-01');
+INSERT INTO car_booking(car_id, start_date, end_date) VALUES (1, '2021-01-30', '2021-06-27'), (2, '2021-04-30', '2021-06-27'), (3, '2021-05-30', '2021-06-27'), (4, '2021-06-01', '2021-06-27'), (5, '2021-06-14', '2021-06-27');
